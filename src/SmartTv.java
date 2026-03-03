6f6aabeaaf474eaa319a0c33bf0914ad784ca411
Yasmim <yasmimalsferreira@gmail.com>
2026-03-02 22:04:50 -0300

    Encapsulamento

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -2,6 +2,37 @@ public class SmartTv {
     private String marca, modelo;
     private int volume;
 
+    public SmartTv(){ // SmartTv obj1 = new Smart()
+
+
+    }
+    public SmartTv(String marca, String modelo, int volume){
+        this.marca = marca;
+        this.modelo = modelo;
+        this. setVolume(volume);
+    }
+    // setters e getters
+    public void setVolume(int volume){
+        if (volume >= 0 && volume <= 100){
+            this.volume = volume;
+        }
+        else System.out.println("Volume inválido");
+    }
+    public int getVolume(){
+        return this.volume;
+    }
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    public String getMarca(){
+        return this.marca;
+    }
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    public String getModelo(){
+        return this.modelo;
+    }
 }
 
 
--- src/TesteSmartTv.java
+++ src/TesteSmartTv.java
@@ -1,3 +1,11 @@
 public class TesteSmartTv {
+    public static void main(String[] args){
+        SmartTv obj1 = new SmartTv();
 
+        obj1.setVolume(120); // não vai dar certo
+        obj1.setVolume(30); // vai dar certo
+
+        System.out.println("Volume " + obj1.getVolume());
+        SmartTv obj2 = new SmartTv("Samsung","QLed 32", 0);
+    }
 }
