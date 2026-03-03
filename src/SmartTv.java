f9b04427bcdfd23cd25c3927819cbbf51adf838e
Yasmim <yasmimalsferreira@gmail.com>
2026-03-02 21:41:15 -0300

    Aula 01 - Encapsulamento

---
+++ src/SmartTv.java
@@ -0,0 +1,20 @@
+public class SmartTv {
+    private String marca, modelo;
+    private int volume;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Encapsulamento é guardar para proteger, o meu objetivo é esconder para prevenir mau uso
+// podemos encapsular variaveis e metódos
+// para encapsular variavel é proibido usar public e vão passar a ser private
